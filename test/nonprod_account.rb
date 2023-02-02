5ecca8b9368b25516be2e660546c8e33f00af8d7
Richard Lau <rilau@thoughtworks.com>
2023-02-02 16:40:47 -0500

    Fix tests

--- test/nonprod_account.rb
+++ test/nonprod_account.rb
@@ -1,13 +1,21 @@
 require 'awspec'
 
-describe route53_hosted_zone('cdicohorts.red') do
+describe route53_hosted_zone('cdicohorts.red.') do
   it { should exist }
 end
 
-describe route53_hosted_zone('dev-us-east-1.twdps.io.') do
+describe route53_hosted_zone('dev.cdicohorts.red.') do
   it { should exist }
 end
 
-describe route53_hosted_zone('dev-us-east-1.cdicohorts.red.') do
+describe route53_hosted_zone('nonprod-us-east-2.cdicohorts.red.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('prod.cdicohorts.red.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('qa.cdicohorts.red.') do
   it { should exist }
 end
